f3aaf7ab8475f7e283311efa867c3e2a129af600
Jeancarlo <jcode2006@gmail.com>
2022-03-01 17:32:32 -0500

    ♻️

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,42 @@
 /* **************** Imports **************** */
 // Disney+ Header
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import logo from '../assets/svg/logo.svg'
-import FilmSVG from './svgs/FilmSVG'
-import HouseSVG from './svgs/HouseSVG'
-import PlusSVG from './svgs/PlusSvg'
-import SearchSVG from './svgs/SearchSVG'
-import StarSVG from './svgs/StarSVG'
-import TVSVG from './svgs/TVSVG'
 import styles from '../styles/Header.css'
+import Navigation from './Navigation'
 
 /* **************** Variables **************** */
 
 /* **************** Functions **************** */
 const Header = () => {
+  const [user, setUser] = useState(null)
+
+  const handleLogout = () => {
+    setUser(null)
+  }
+  const handleLogin = (user) => {
+    setUser(user)
+  }
+
   return (
     <header className='header'>
       <div className='header-logo'>
         <img src={logo} alt='logo' />
       </div>
 
-      <div className='header-nav'>
-        <NavLink to='/'>
-          <HouseSVG />
-          <span>Home</span>
-        </NavLink>
-        <NavLink to='/search'>
-          <SearchSVG />
-          <span>Search</span>
-        </NavLink>
-        <NavLink to='/mylist'>
-          <PlusSVG />
-          <span>My List</span>
-        </NavLink>
-        <NavLink to='/originals'>
-          <StarSVG />
-          <span>Originals</span>
-        </NavLink>
-        <NavLink to='/movies'>
-          <FilmSVG />
-          <span>Movies</span>
-        </NavLink>
-        <NavLink to='/series'>
-          <TVSVG />
-          <span>Series</span>
-        </NavLink>
-      </div>
+      <Navigation />
 
       <div className='header-user'>
-        <img
-          src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200&r=pg&d=mm'
-          alt='user'
-        />
+        {user ? (
+          <>
+            <span>{user.name}</span>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <button onClick={() => handleLogin({ name: 'John Doe' })}>
+            Login
+          </button>
+        )}
       </div>
     </header>
   )
